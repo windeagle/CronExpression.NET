6f0f65a5d205a97a9b6ccae9be78d125004bcde4
jinyu.yin <jinyu.yin@dianping.com>
2013-03-20 15:51:08 +0800

    Create the ICronCheck interface.

---
+++ ICronCheck.cs
@@ -0,0 +1,15 @@
+﻿namespace CronExpression.NET
+{
+    /// <summary>
+    /// 用于“检查给定的值是否满足 Cron 表达式的对应部分所包含的条件”的接口
+    /// </summary>
+    public interface ICronCheck
+    {
+        /// <summary>
+        /// 检查给定的值是否满足 Cron 表达式的对应部分所包含的条件
+        /// </summary>
+        /// <param name="datePartValue">日期时间类型的指定部分的值</param>
+        /// <returns>是否满足 Cron 表达式条件</returns>
+        bool Check(int datePartValue);
+    }
+}
