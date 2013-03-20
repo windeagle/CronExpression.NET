f68c250a61eec1241ea684516cd7853d9d32a6e3
jinyu.yin <jinyu.yin@dianping.com>
2013-03-20 18:20:05 +0800

    add MonthCronCheck class and spell error.

--- MonthCronCheck.cs
+++ MonthCronCheck.cs
@@ -1,19 +1,19 @@
 ﻿namespace CronExpression.NET
 {
     /// <summary>
-    /// 用于“检查给定的年份值是否满足 Cron 表达式的年份部分所包含的条件”的 ICronCheck 实现类
+    /// 用于“检查给定的月份值是否满足 Cron 表达式的月份部分所包含的条件”的 ICronCheck 实现类
     /// </summary>
-    public class YearCronCheck : BaseCronCheck, ICronCheck
+    public class MonthCronCheck : BaseCronCheck, ICronCheck
     {
         /// <summary>
-        /// 检查给定的年份值是否满足 Cron 表达式的年份部分所包含的条件
+        /// 检查给定的月份值是否满足 Cron 表达式的月份部分所包含的条件
         /// </summary>
         /// <param name="datePartValue"></param>
         /// <returns></returns>
         public bool Check(int datePartValue)
         {
             //在调用基类的基础条件检查之前先检查给定值的范围
-            if (datePartValue < 1970 || datePartValue > 2099) return false;
+            if (datePartValue < 1 || datePartValue > 12) return false;
             return BaseCheck(datePartValue);
         }
     }
